4cef8a6b8a8f032bce1c54e03262d13f6a1ebc55
Paweł Bytner <pawel.m.bytner@gmail.com>
2024-03-03 13:52:36 +0100

    Dodaj pliki projektów.

---
+++ App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace P_NET_24_WZ_ININ4_PR
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
